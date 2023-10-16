a029123487bfac1c438c04ed49fdb067f8fc08db
Saiful Amin <saiful.aminx@gmail.com>
2023-10-16 20:20:58 +0700

    membuat routing untuk expressJS

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import express from 'express';
 
-module.exports = {
-    routePrefix: () => {
-        return express.Router()
-    },
-    group: (callback) => {
-        const router = express.Router()
-        callback(router)
-        return router;
-    }
+export const apiRouter = express.Router();
+
+export const group = (callback) => {
+    const router = express.Router()
+    callback(router)
+    return router;
 };
