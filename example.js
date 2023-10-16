8633f875a7027a09a5d4dced2a8c3b5b034ab153
Saiful Amin <saiful.aminx@gmail.com>
2023-10-16 20:41:42 +0700

    perbaikan untuk membuat extension

---
+++ example.js
@@ -0,0 +1,8 @@
+import {prefixRoute, group} from 'group-route-express'
+
+prefixRoute.use('/post', group((router) => {
+    router.get('/', function (req, res) {
+        res.send("Hallo World");
+    });
+    // router.get('test', yourControllerAndFunction)
+}))
